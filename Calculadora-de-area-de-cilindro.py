40fbe849355b3fbe295307415c6676d58af32e94
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:26:28 -0600

    Create Calculadora-de-area-de-cilindro.py

---
+++ Calculadora-de-area-de-cilindro.py
@@ -0,0 +1,8 @@
+print("Programa de volumen de un cilindro")
+#aqui vamos a pedir la variable de la altura y del radio
+n=int(input("Ingrese la altura del cilindro: " ))
+n1=int(input("Ingrese el radio  del cilindro: " ) )
+#aqui lo que haremos es aplicar la formula del volumen de un cilindro que es pi por radio a la dos por la altura
+n2=3.14*(n1*n1)*n
+#aqui mostraremos el resultado 
+print("el el volumen es de ", n2)

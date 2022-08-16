85eeef3dd652b83bdd37ea4a37db57bbd8d7589c
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:28:34 -0600

    Create Parte-2-4.py

---
+++ Parte-2-4.py
@@ -0,0 +1,7 @@
+x=int(input("Ingrese el primer numero"))
+y=int(input("Ingrese el segundo numero"))
+
+p=x**(1/y)
+print("la respuesta es", p)
+
+#aqui lo q hace es encontrar la raiz r de x diviendolo entre uno y luego multiplicandolo 

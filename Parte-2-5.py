c9f5818a9be90c56dad6c84e94ee4920c0ec9ea0
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:28:51 -0600

    Create Parte-2-5.py

---
+++ Parte-2-5.py
@@ -0,0 +1,16 @@
+y=str(input("Ingrese una palabra "))
+x=y[0]
+if x == "a":
+  print("inicia con una vocal ")
+elif x == "e":
+  print("inicia con una vocal ")
+elif x == "i":
+  print("inicia con una vocal ")
+elif x == "o":
+  print("inicia con una vocal ")
+elif x == "u":
+  print("inicia con una vocal ")
+else:
+  print("inicia con una consonante ")
+
+#ponemos diferentes condiciones con solamente las vocales para se que mas facil 

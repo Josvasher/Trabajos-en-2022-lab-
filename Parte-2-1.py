a3d8222b81cc15103469b502b3da635fd8909487
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:29:52 -0600

    Create Parte-2-1.py

---
+++ Parte-2-1.py
@@ -0,0 +1,9 @@
+lista=[]
+for i in range (0,10):
+  x=int(input("Ingrese un numero hasta llegar a poner 10 numeros "))
+  #pedimos poner en cada vez una nueva cariable y volverla a agregarla  en la lista
+  lista.append(x)
+print((lista[1])+(lista[9]))
+print((lista[2])+(lista[8]))
+
+#aqui repetimos un rango de 1-10 para pedir los numeros que queremos poniendo la posicion del numero en la lista

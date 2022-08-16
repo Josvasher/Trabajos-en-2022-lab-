8fca1b98b68418158ae06f11d9a45973b290d852
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:18:41 -0600

    Create Ejercicio-4.py

---
+++ Ejercicio-4.py
@@ -0,0 +1,18 @@
+#este programa diria un mensaje dependiendo el dia que escojan
+dia=str(input("Ingrese el dia de la semana sin mayusculas "))
+if dia == "lunes":
+  print("Ten un feliz lunes ")
+elif dia == "martes":
+  print("Ten un feliz martes ")
+elif dia == "miercoles":
+  print("Ten un feliz miercoles ")
+elif dia == "jueves":
+  print("Ten un feliz jueves") 
+elif dia == "viernes":
+  print("Ten un feliz viernes ")
+elif dia == "sabado":
+  print("Ten un feliz sabado")
+elif dia == "domingo":
+  print("Ten un feliz domingo ")
+else:
+  print("no has ingresado un dia correcto, vuelve a hacerlo ")

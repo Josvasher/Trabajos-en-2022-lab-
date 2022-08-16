cf77b8f1dbb5170eccaf318526122929d49dcd5b
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:18:08 -0600

    Create Ejericio-5.py

---
+++ Ejericio-5.py
@@ -0,0 +1,11 @@
+#encuentra el valor absoluto 
+
+n1=int(input("Ingrese el primer numero "))
+
+if n1 > 1: 
+  print("el valor absoluto de", n1, "es ", n1 )
+elif 1 > n1: 
+  n2 = n1 * -1
+  print("el valor absoluto de", n1, "es ", n2)
+else:
+  print("el valor absoluto de", n1, "es ", n1)

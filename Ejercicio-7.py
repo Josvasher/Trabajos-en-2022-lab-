dd0766e60829b45831c150084ee99c63eba7dd84
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:16:01 -0600

    Create Ejercicio-7.py

---
+++ Ejercicio-7.py
@@ -0,0 +1,12 @@
+#este programa es un mini menu sobre votaciones 
+print("Elecciones a presidente ")
+ele=str(input("Elija el partido cual usted vota: A. Rojo B. Verde C. Azul "))
+if ele == "A":
+  print("Su voto ha sido realizado para el partido Rojo ")
+elif ele == "B":
+  print("Su voto ha sido realizado para el partido verde ")
+elif ele == "C":
+  print("Su voto ha sido realizado para el partido azul ")
+else: 
+  print("Opcion erronea, vuelva a realizar el voto")
+        

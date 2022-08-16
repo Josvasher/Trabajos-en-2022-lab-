1b3b7921eef91db1fc7826fc71d76e7e4d2068da
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:11:06 -0600

    Create Bucle-de-ingresar-contraseña.py

---
+++ Bucle-de-ingresar-contraseña.py
@@ -0,0 +1,8 @@
+#en este programa hay un bucle que solamente pregunta sobre una contraseña 
+
+n2=str("hola")
+n1=str
+while n1 != n2:
+  n1=str(input("Ingrese la contraseña "))
+else:
+  print("Contraseña correcta ")

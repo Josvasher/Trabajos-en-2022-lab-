4dc8a8f8785b6848b683f7e89bb29f6b3b4d50c6
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:12:18 -0600

    Create NUmeros-pares.py

---
+++ NUmeros-pares.py
@@ -0,0 +1,6 @@
+#este progama pregunta que hasta que numero muestre cuales son numeros impares 
+n1=2
+n2=int(input("Ingrese el limite de la lista de numero de numeros pares: "))
+while n1 <= n2:
+  print(n1)
+  n1 +=2

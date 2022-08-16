1ecc9905a9a7b24c2195cc453baf98dfc24903eb
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:09:14 -0600

    Create Ejemplo-de-operadores.py

---
+++ Ejemplo-de-operadores.py
@@ -0,0 +1,26 @@
+#este programa realiza diferentes operaciones como ejemplo para ver como funciona diferentes operadores 
+
+print ("Ejemplos de operadores ")
+
+n1=4
+n2=3
+
+print("Esto es una suma")
+res=n1+n2
+print (res)
+
+print("Esto es una resta")
+res=n1-n2
+print (res)
+
+print("Esto es una multiplicacion")
+res=n1*n2
+print (res)
+
+print("Esto es una division")
+res=n1/n2
+print (res)
+
+print("Esto es un modulo")
+res=n1%n2
+print (res)

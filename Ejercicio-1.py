67fd4855f8762c296f479b91acb41156a7216a4d
Josvasher <111383294+Josvasher@users.noreply.github.com>
2022-08-16 12:22:37 -0600

    Create Ejercicio-1.py

---
+++ Ejercicio-1.py
@@ -0,0 +1,7 @@
+#este programa es un pequeño ejemplo de un menu paa saber si tiene el mismo numero de loteria que el boleto ganador 
+
+premio=int(input("Cual es el numero de su boleto de loteria? "))
+if premio == 1000: 
+  print("Eres el ganor del premio")
+else:
+  print("Mala suerte, no eres el ganador")
